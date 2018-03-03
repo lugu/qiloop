0ef6b199f9ec84e52eacf9f654bfefaa4501d41d
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-03-03 13:20:43 +0100

    serialisation test for MetaObject

---
+++ src/metaqi/object/object_test.go
@@ -0,0 +1,40 @@
+package object_test
+
+import (
+	"bytes"
+	"metaqi/object"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func helpReadGolden(t *testing.T) object.MetaObject {
+	path := filepath.Join("testdata", "metaObject-reply-data.bin")
+	file, err := os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	metaObj, err := object.ReadMetaObject(file)
+	if err != nil {
+		t.Errorf("failed to read MetaObject: %s", err)
+	}
+	return metaObj
+}
+
+func TestReadMetaObject(t *testing.T) {
+	helpReadGolden(t)
+}
+
+func TestReadWriteMetaObject(t *testing.T) {
+	metaObj := helpReadGolden(t)
+	buf := bytes.NewBuffer(make([]byte, 0))
+	if err := object.WriteMetaObject(metaObj, buf); err != nil {
+		t.Errorf("failed to write MetaObject: %s", err)
+	}
+	if metaObj2, err := object.ReadMetaObject(buf); err != nil {
+		t.Errorf("failed to re-read MetaObject: %s", err)
+	} else if !reflect.DeepEqual(metaObj, metaObj2) {
+		t.Errorf("expected %#v, got %#v", metaObj, metaObj2)
+	}
+}
