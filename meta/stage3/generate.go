dcf1b62d3530c97c27f1d86cd5ce65ee87ae2719
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-03-05 22:22:34 +0100

    travis build

--- meta/stage3/generate.go
+++ meta/stage3/generate.go
@@ -1,4 +1,4 @@
-//go:generate go get github.com/lugu/qiloop/meta/cmd/stage3
+//go:generate go get github.com/lugu/qiloop/meta/stage3/cmd/stage3
 //go:generate $GOPATH/bin/stage3 meta-object.bin directory.go
 
 package stage3
