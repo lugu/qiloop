e285806d1f2a0fe86e8772811e6bd2396a9a1790
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-03-09 21:21:53 +0100

    meta/stage/generate.go replaced with object/generate.go

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,12 @@
 set -e
 
 rm -f \
-    meta/stage1/metaobject.go \
     meta/stage2/services.go   \
     meta/stage3/services.go   \
     services/services.go      \
     object/metaobject.go      \
 
 go get -d github.com/lugu/qiloop/...
-go generate github.com/lugu/qiloop/meta/stage1
 go generate github.com/lugu/qiloop/object
 go generate github.com/lugu/qiloop/meta/stage2
 go generate github.com/lugu/qiloop/meta/stage3
--- meta/stage1/generate.go
+++
@@ -1,4 +0,0 @@
-//go:generate go get github.com/lugu/qiloop/meta/stage1/cmd/stage1
-//go:generate $GOPATH/bin/stage1 stage1 metaobject.go
-
-package object
