795eece66dd07d72a99afe8c11e7e3dc5aaf2526
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-03-10 23:51:55 +0100

    info implements: qicli info <service>

--- cmd/info/main.go
+++ cmd/info/main.go
@@ -3,29 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/lugu/qiloop/object"
 	"github.com/lugu/qiloop/services"
 	"github.com/lugu/qiloop/session/dummy"
 	"log"
+	"os"
 )
+
+func Print(i interface{}) {
+	json, err := json.MarshalIndent(i, "", "    ")
+	if err != nil {
+		log.Fatalf("json encoding failed: %s", err)
+	}
+	fmt.Println(string(json))
+}
 
 func main() {
 	sess, err := dummy.NewSession(":9559")
 	if err != nil {
 		log.Fatalf("failed to connect: %s", err)
 	}
-	directory, err := services.NewServiceDirectory(sess, 1)
-	if err != nil {
-		log.Fatalf("directory creation failed: %s", err)
-	}
 
-	services, err := directory.Services()
-	if err != nil {
-		log.Fatalf("failed to list services: %s", err)
+	if len(os.Args) > 1 {
+		serviceName := os.Args[1]
+		proxy, err := sess.Proxy(serviceName, 1)
+		if err != nil {
+			log.Fatalf("failed to connect service (%s): %s", serviceName, err)
+		}
+		var obj object.Object = &services.Object{proxy}
+		meta, err := obj.MetaObject(1)
+		if err != nil {
+			log.Fatalf("failed to get metaobject (%s): %s", serviceName, err)
+		}
+		Print(meta)
+	} else {
+		directory, err := services.NewServiceDirectory(sess, 1)
+		if err != nil {
+			log.Fatalf("directory creation failed: %s", err)
+		}
+		services, err := directory.Services()
+		if err != nil {
+			log.Fatalf("failed to list services: %s", err)
+		}
+		Print(services)
 	}
-
-	json, err := json.MarshalIndent(services, "", "    ")
-	if err != nil {
-		log.Fatalf("json encoding failed: %s", err)
-	}
-	fmt.Println(string(json))
 }
