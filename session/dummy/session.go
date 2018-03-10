bbc4c086de9f77e6af00b9242bc9591190471c02
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-03-10 11:55:01 +0100

    session: authenticate before calling metaObject

--- session/dummy/session.go
+++ session/dummy/session.go
@@ -3,16 +3,23 @@ package dummy
 import (
 	"fmt"
 	"github.com/lugu/qiloop/net"
+	"github.com/lugu/qiloop/object"
 	"github.com/lugu/qiloop/services"
 	"github.com/lugu/qiloop/session"
 	"github.com/lugu/qiloop/value"
 	"strings"
 )
 
-func Authenticate(e net.EndPoint) error {
-	server0 := services.Server{
-		manualProxy(e, 0, 0),
-	}
+func Authenticate(endpoint net.EndPoint) error {
+
+	const serviceID = 0
+	const objectID = 0
+	const messageID = 0
+
+	client0 := &blockingClient{endpoint, messageID}
+	proxy0 := NewProxy(client0, object.MetaService0, serviceID, objectID)
+	server0 := services.Server{proxy0}
+
 	permissions := map[string]value.Value{
 		"ClientServerSocket":    value.Bool(true),
 		"MessageFlags":          value.Bool(true),
@@ -32,26 +39,6 @@ type staticSession struct {
 	services []services.ServiceInfo
 }
 
-func (d *staticSession) Proxy(name string, objectID uint32) (p session.Proxy, err error) {
-
-	for _, service := range d.services {
-		if service.Name == name {
-			return newServiceProxy(service, objectID)
-		}
-	}
-	return p, fmt.Errorf("service not found: %s", name)
-}
-
-// manualProxy is to create proxies to the directory and server
-// services needed for a session.
-func manualProxy(e net.EndPoint, serviceID, objectID uint32) session.Proxy {
-	client := &blockingClient{e, 3}
-	meta, err := session.MetaObject(client, serviceID, objectID)
-	if err != nil {
-	}
-	return NewProxy(client, meta, serviceID, objectID)
-}
-
 func newServiceProxy(info services.ServiceInfo, objectID uint32) (p session.Proxy, err error) {
 
 	if len(info.Endpoints) == 0 {
@@ -68,7 +55,28 @@ func newServiceProxy(info services.ServiceInfo, objectID uint32) (p session.Prox
 	}
 
 	// FIXME: object id do be defined
-	return manualProxy(endpoint, info.ServiceId, objectID), nil
+	return metaProxy(endpoint, info.ServiceId, objectID), nil
+}
+
+func (d *staticSession) Proxy(name string, objectID uint32) (p session.Proxy, err error) {
+
+	for _, service := range d.services {
+		if service.Name == name {
+			return newServiceProxy(service, objectID)
+		}
+	}
+	return p, fmt.Errorf("service not found: %s", name)
+}
+
+// metaProxy is to create proxies to the directory and server
+// services needed for a session.
+func metaProxy(e net.EndPoint, serviceID, objectID uint32) session.Proxy {
+	client := &blockingClient{e, 3}
+	// FIXME: request a meta object
+	meta, err := session.MetaObject(client, serviceID, objectID)
+	if err != nil {
+	}
+	return NewProxy(client, meta, serviceID, objectID)
 }
 
 func NewSession(addr string) (s *staticSession, err error) {
@@ -82,7 +90,7 @@ func NewSession(addr string) (s *staticSession, err error) {
 	}
 
 	directory := services.ServiceDirectory{
-		manualProxy(endpoint, 1, 1),
+		metaProxy(endpoint, 1, 1),
 	}
 	s = new(staticSession)
 	s.services, err = directory.Services()
