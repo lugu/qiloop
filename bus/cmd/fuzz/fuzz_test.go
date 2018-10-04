e97ae834b447a58187152277f7826f4bc523f4d5
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-10-04 20:01:19 +0200

    bus/cmd/fuzz: complete tests

--- bus/cmd/fuzz/fuzz_test.go
+++ bus/cmd/fuzz/fuzz_test.go
@@ -2,20 +2,41 @@ package fuzz_test
 
 import (
 	"github.com/lugu/qiloop/bus/cmd/fuzz"
+	"github.com/lugu/qiloop/bus/session"
 	"io/ioutil"
+	gonet "net"
 	"path/filepath"
 	"testing"
 )
 
 func TestFuzz(t *testing.T) {
 	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("panic with %s", r)
+		if r := recover(); r == nil {
+			t.Errorf("shall panic")
 		}
 	}()
+
+	passwords := map[string]string{
+		"nao": "nao",
+	}
+	object := session.NewServiceAuthenticate(passwords)
+	ns := session.NewNamespace(object)
+	router := session.NewRouter()
+	router.Add(ns)
+	listener, err := gonet.Listen("tcp", ":9559")
+	if err != nil {
+		panic(err)
+	}
+	server := session.NewServer(listener, router)
+	go server.Run()
+
+	fuzz.ServerURL = "tcp://localhost:9559"
+
 	data, err := ioutil.ReadFile(filepath.Join("testdata", "cap-auth-failure.bin"))
 	if err != nil {
 		t.Errorf("cannot open test data %s", err)
 	}
 	fuzz.Fuzz(data)
+
+	server.Stop()
 }
