6de43c51edad741ddc31edd0a5948205f9f353dd
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-10-04 20:01:01 +0200

    stb example

---
+++ bus/services/stub/directory.go
@@ -0,0 +1,69 @@
+package stub
+
+import (
+	"bytes"
+	"github.com/lugu/qiloop/bus"
+	"github.com/lugu/qiloop/bus/services"
+	"github.com/lugu/qiloop/bus/services/impl"
+	"github.com/lugu/qiloop/bus/session"
+	"github.com/lugu/qiloop/type/basic"
+	"github.com/lugu/qiloop/type/value"
+)
+
+// ServiceDirectoryStub implements session.Object
+type ServiceDirectoryStub struct {
+	session.ObjectDispather
+	impl impl.ServiceDirectoryInterface
+}
+
+func NewServiceDirectory(impl impl.ServiceDirectoryInterface) session.Object {
+	var stub ServiceDirectoryStub
+	stub.Wrapper = bus.Wrapper(make(map[uint32]bus.ActionWrapper))
+
+	stub.Wrapper[1] = nil // FIXME: name of the corresponding method
+	// of ServiceDirectoryStub
+	return &stub
+}
+
+// FIXME: move ino utils
+func ErrorPaylad(err error) []byte {
+	buf := bytes.NewBuffer(make([]byte, 0))
+	errV := value.String(err.Error())
+	errV.Write(buf)
+	return buf.Bytes()
+}
+
+// Service unmarshall the argument, call the real implementation
+// and marshall the response
+func (s ServiceDirectoryStub) Service(payload []byte) ([]byte, error) {
+
+	buf := bytes.NewBuffer(payload)
+	serviceID, err := basic.ReadString(buf)
+	if err != nil {
+		return ErrorPaylad(err), nil
+	}
+
+	info, err := s.impl.Service(serviceID)
+	if err != nil {
+		return ErrorPaylad(err), nil
+	}
+
+	buf = bytes.NewBuffer(make([]byte, 0))
+	err = services.WriteServiceInfo(info, buf)
+	if err != nil {
+		return ErrorPaylad(err), nil
+	}
+
+	return buf.Bytes(), nil
+}
+
+// type ServiceDirectoryInterface interface {
+// 	Service(P0 string) (ServiceInfo, error)
+// 	Services() ([]ServiceInfo, error)
+// 	RegisterService(P0 ServiceInfo) (uint32, error)
+// 	UnregisterService(P0 uint32) error
+// 	ServiceReady(P0 uint32) error
+// 	UpdateServiceInfo(P0 ServiceInfo) error
+// 	MachineId() (string, error)
+// 	_socketOfService(P0 uint32) (object.ObjectReference, error)
+// }
