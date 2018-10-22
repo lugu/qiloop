24e203061f71bcc7bf6d9fae4803e30baf7773d6
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-10-22 23:55:54 +0200

    bus/session: test service zero

---
+++ bus/session/authenticate_test.go
@@ -0,0 +1,56 @@
+package session_test
+
+import (
+	"github.com/lugu/qiloop/bus/client"
+	"github.com/lugu/qiloop/bus/net"
+	"github.com/lugu/qiloop/bus/session"
+	"github.com/lugu/qiloop/bus/util"
+	gonet "net"
+	"testing"
+)
+
+func helpAuth(t *testing.T, creds map[string]string, user, token string, ok bool) {
+	name := util.MakeTempFileName()
+
+	listener, err := gonet.Listen("unix", name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	object := session.NewServiceAuthenticate(creds)
+
+	ns := session.NewNamespace(object)
+	router := session.NewRouter()
+	router.Add(ns)
+	server := session.NewServer(listener, router)
+	go server.Run()
+
+	ep, err := net.DialEndPoint("unix://" + name)
+	if err != nil {
+		panic(err)
+	}
+	err = client.AuthenticateUser(ep, user, token)
+	if ok && err != nil {
+		t.Errorf("user: %s, token: %s, error: %s", user, token, err)
+	}
+	if !ok && err == nil {
+		t.Errorf("shall not pass: %s, %s", user, token)
+	}
+}
+
+func TestNewServiceAuthenticate(t *testing.T) {
+
+	credentials := map[string]string{
+		"foo":  "bar",
+		"bazz": "bozz",
+	}
+	helpAuth(t, credentials, "foo", "bar", true)
+	helpAuth(t, credentials, "foo", "", false)
+	helpAuth(t, credentials, "", "bar", false)
+	helpAuth(t, credentials, "", "", false)
+	helpAuth(t, credentials, "user", "pass", false)
+	helpAuth(t, credentials, "user", "", false)
+	helpAuth(t, credentials, "", "pass", false)
+	helpAuth(t, credentials, "bazz", "", false)
+	helpAuth(t, credentials, "bazz", "bozz", true)
+}
