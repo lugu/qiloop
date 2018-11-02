3590ebd80cf969c4c33ba14e1db2f772d5309cbd
Ludovic Guegan <lguegan@aldebaran-robotics.com>
2018-11-02 14:03:11 +0100

    cmd/stub: move command here

--- meta/stub/generate.go
+++ meta/stub/generate.go
@@ -1,4 +1,4 @@
-//go:generate go get github.com/lugu/qiloop/meta/stub/cmd/stub
+//go:generate go get github.com/lugu/qiloop/cmd/stub
 //go:generate $GOPATH/bin/stub -idl testdata/object.idl -output generate_test.go
 
 package stub
