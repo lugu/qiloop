48baf16d34d184e93ec5ee0f7e6e6eaf4755159f
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-11-08 22:29:06 +0100

    bus/server/directory: NewServer interface

--- bus/server/directory/directory_test.go
+++ bus/server/directory/directory_test.go
@@ -2,8 +2,6 @@ package directory_test
 
 import (
 	proxy "github.com/lugu/qiloop/bus/client/services"
-	"github.com/lugu/qiloop/bus/net"
-	srv "github.com/lugu/qiloop/bus/server"
 	dir "github.com/lugu/qiloop/bus/server/directory"
 	sess "github.com/lugu/qiloop/bus/session"
 	"github.com/lugu/qiloop/bus/util"
@@ -14,40 +12,10 @@ import (
 func TestNewServer(t *testing.T) {
 	name := util.MakeTempFileName()
 
-	listener, err := net.Listen("unix://" + name)
+	server, err := dir.NewServer("unix://" + name)
 	if err != nil {
 		t.Fatal(err)
 	}
-	router := srv.NewRouter(srv.NewServiceAuthenticate(make(map[string]string)))
-
-	impl := dir.NewServiceDirectory()
-	info := dir.ServiceInfo{
-		Name:      "ServiceDirectory",
-		ServiceId: 1,
-		MachineId: util.MachineID(),
-		ProcessId: util.ProcessID(),
-		Endpoints: []string{"unix://" + name},
-		SessionId: "", // TODO
-	}
-	serviceID, err := impl.RegisterService(info)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if serviceID != 1 {
-		t.Fatalf("service directory id: %d", serviceID)
-	}
-	err = impl.ServiceReady(1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	object := dir.ServiceDirectoryObject(impl)
-	_, err = router.Add(srv.NewService(object))
-	if err != nil {
-		t.Fatal(err)
-	}
-	server := srv.StandAloneServer(listener, router)
-
 	go server.Run()
 
 	session, err := sess.NewSession("unix://" + name)
---
+++ bus/server/directory/standalone.go
@@ -0,0 +1,46 @@
+package directory
+
+import (
+	"fmt"
+	"github.com/lugu/qiloop/bus/net"
+	"github.com/lugu/qiloop/bus/server"
+	"github.com/lugu/qiloop/bus/util"
+)
+
+func NewServer(addr string) (*server.Server, error) {
+
+	router := server.NewRouter(server.NewServiceAuthenticate(make(map[string]string)))
+
+	impl := NewServiceDirectory()
+	info := ServiceInfo{
+		Name:      "ServiceDirectory",
+		ServiceId: 1,
+		MachineId: util.MachineID(),
+		ProcessId: util.ProcessID(),
+		Endpoints: []string{addr},
+		SessionId: "",
+	}
+	serviceID, err := impl.RegisterService(info)
+	if err != nil {
+		return nil, err
+	}
+	if serviceID != 1 {
+		return nil, fmt.Errorf("service directory id: %d", serviceID)
+	}
+	err = impl.ServiceReady(1)
+	if err != nil {
+		return nil, err
+	}
+
+	object := ServiceDirectoryObject(impl)
+	_, err = router.Add(server.NewService(object))
+	if err != nil {
+		return nil, err
+	}
+
+	listener, err := net.Listen(addr)
+	if err != nil {
+		return nil, err
+	}
+	return server.StandAloneServer(listener, router), nil
+}
