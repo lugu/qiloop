ad364d434a211d85418dd662008322d6c8935c22
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-11-22 14:00:00 +0100

    bus/net/cert: better error message

--- bus/net/cert/cert.go
+++ bus/net/cert/cert.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"io"
 	"math/big"
 	"os"
@@ -38,31 +39,34 @@ func GenerateCertificate() (tls.Certificate, error) {
 	return tls.X509KeyPair(certPEM, keyPEM)
 }
 
-func Certificate() (tls.Certificate, error) {
-	certFile, keyFile := getCertFiles()
+func Certificate() (cert tls.Certificate, err error) {
+	certFile, keyFile, err := getCertFiles()
+	if err != nil {
+		return cert, err
+	}
 	return tls.LoadX509KeyPair(certFile, keyFile)
 }
 
 // getCertFiles returns the public and an X509 certificate and the
 // associated private RSA key
-func getCertFiles() (string, string) {
+func getCertFiles() (string, string, error) {
 	usr, err := user.Current()
 	if err != nil {
-		return "", ""
+		return "", "", fmt.Errorf("cannot get home directory")
 	}
 	file, err := os.Open(usr.HomeDir + "/.qi-cert.conf")
 	if err != nil {
-		return "", ""
+		return "", "", fmt.Errorf("cannot open $HOME/.qi-cert.conf")
 	}
 	defer file.Close()
 	r := bufio.NewReader(file)
 	cert, err := r.ReadString('\n')
 	if err != nil {
-		return "", ""
+		return "", "", fmt.Errorf("cannot read cert from $HOME/.qi-cert.conf")
 	}
 	key, err := r.ReadString('\n')
 	if err != io.EOF && err != nil {
-		return "", ""
+		return "", "", fmt.Errorf("cannot read key from $HOME/.qi-cert.conf")
 	}
-	return strings.TrimSpace(cert), strings.TrimSpace(key)
+	return strings.TrimSpace(cert), strings.TrimSpace(key), nil
 }
