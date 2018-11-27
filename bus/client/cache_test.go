7bae083e5c1ddebed65cc222d0c50d53af17437b
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-11-27 13:39:29 +0100

    bus/client: test proxy gen

--- bus/client/cache_test.go
+++ bus/client/cache_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lugu/qiloop/bus/server/directory"
 	"github.com/lugu/qiloop/bus/util"
 	"github.com/lugu/qiloop/type/object"
+	"github.com/lugu/qiloop/type/value"
 	gonet "net"
 	"strings"
 	"testing"
@@ -32,6 +33,41 @@ func TestCache(t *testing.T) {
 
 	err = cache.Lookup("ServiceDirectory", 1)
 	if err == nil {
+		panic("expecting an authentication error")
+	}
+}
+
+func TestServerProxy(t *testing.T) {
+	addr := util.NewUnixAddr()
+
+	server, err := directory.NewServer(addr, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Terminate()
+
+	endpoint, err := net.DialEndPoint(addr)
+	if err != nil {
+		panic(err)
+	}
+	cache := client.NewCache(endpoint)
+	defer cache.Destroy()
+
+	_, err = client.NewServer(cache, 0)
+	if err == nil {
+		panic("shall not create service not in the cache")
+	}
+
+	cache.AddService("Server", 0, object.MetaService0)
+
+	_, err = client.NewServer(cache, 0)
+	if err != nil {
+		panic("expecting an authentication error")
+	}
+
+	services := client.Services(cache)
+	_, err = services.Server()
+	if err != nil {
 		panic("expecting an authentication error")
 	}
 }
@@ -93,5 +129,38 @@ func TestCacheAuthError(t *testing.T) {
 	_, err = client.NewCachedSession(addr)
 	if err == nil {
 		panic("expecting an error")
+	}
+}
+
+func TestServerError(t *testing.T) {
+	clt, srv := gonet.Pipe()
+
+	buf := make([]byte, 10)
+
+	go func() {
+		_, err := srv.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		srv.Close()
+	}()
+
+	cache := client.NewCache(net.NewEndPoint(clt))
+	defer cache.Destroy()
+	cache.AddService("Server", 0, object.MetaService0)
+
+	services := client.Services(cache)
+	server0, err := services.Server()
+	if err != nil {
+		panic("expecting an authentication error")
+	}
+
+	capabilityMap := map[string]value.Value{
+		client.KeyUser:  value.String("a"),
+		client.KeyToken: value.String("b"),
+	}
+	_, err = server0.Authenticate(capabilityMap)
+	if err == nil {
+		panic("shall not accept ...")
 	}
 }
