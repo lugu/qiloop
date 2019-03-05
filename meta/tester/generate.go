c8634ea7749b8ab46b35458d5e934aeca12c05f5
Ludovic Guegan <ludovic.guegan@gmail.com>
2019-03-05 23:18:04 +0100

    meta/tester: add stub

--- meta/tester/generate.go
+++ meta/tester/generate.go
@@ -1,4 +1,6 @@
 //go:generate go get github.com/lugu/qiloop/cmd/proxygen
 //go:generate $GOPATH/bin/proxygen -idl tester.idl -output proxy_gen.go
+//go:generate go get github.com/lugu/qiloop/cmd/stub
+//go:generate $GOPATH/bin/stub -idl tester.idl -output stub_gen.go
 
 package tester
