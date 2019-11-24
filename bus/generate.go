e3c31295f9ae6d65bb5b1e1c760ff4e5727bd5f2
Ludovic Guegan <ludovic.guegan@gmail.com>
2019-11-24 23:56:32 +0100

    bus: test object with space example

--- bus/generate.go
+++ bus/generate.go
@@ -1,3 +1,4 @@
 //go:generate go run ../meta/cmd/stub --idl object.idl --output object_stub_gen.go --path github.com/lugu/qiloop/bus
+//go:generate go run ../meta/cmd/stub --idl space_test.idl --output space_stub_gen_test.go
 
 package bus
