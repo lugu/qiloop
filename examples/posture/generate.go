c63734b329c57ee88862a26ed74a8b55ae6d90c4
Ludovic Guegan <ludovic.guegan@gmail.com>
2019-12-02 00:03:10 +0100

    examples/posture: fix generate

--- examples/posture/generate.go
+++ examples/posture/generate.go
@@ -1,3 +1,3 @@
-//go:generate go run ../../meta/cmd/proxy --idl proxy/motion.qi.idl --output proxy/motion_proxy.go
+//go:generate go run ../../meta/cmd/proxy --idl proxy/posture.qi.idl --output proxy/posture_proxy.go
 
 package main
