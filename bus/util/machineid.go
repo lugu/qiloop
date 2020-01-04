2aa87e9474bb71ce2161af527999a5ddcb986aa4
Ludovic Guegan <ludovic.guegan@gmail.com>
2020-01-05 00:19:14 +0100

    bus/util: generate machine id if not supported

--- bus/util/machineid.go
+++ bus/util/machineid.go
@@ -1,18 +1,30 @@
 package util
 
 import (
-	"log"
 	"os"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 
 	"github.com/denisbrodbeck/machineid"
 )
 
+var id string
+
+func init() {
+	var err error
+	id, err = machineid.ProtectedID("qi-messaging")
+	if err != nil {
+		noise := make([]byte, 64)
+		rand.Read(noise)
+		mac := hmac.New(sha256.New, noise)
+		id = hex.EncodeToString(mac.Sum(nil))
+	}
+}
+
 // MachineID returns a fixed ID specific to an application.
 func MachineID() string {
-	id, err := machineid.ProtectedID("qi-messaging")
-	if err != nil {
-		log.Fatalf("OS not supported: failed to read machine-id: %s", err)
-	}
 	return id
 }
 
