f7502832fb338f2aaa14458d2bfb5f27a2dba37f
Ludovic Guegan <ludovic.guegan@gmail.com>
2020-04-14 23:25:21 +0200

    type/conversion: handle slice, map and struct

--- type/conversion/conversion.go
+++ type/conversion/conversion.go
@@ -2,6 +2,7 @@ package conversion
 
 import (
 	"reflect"
+	"strings"
 	"fmt"
 
 	"github.com/lugu/qiloop/type/encoding"
@@ -14,159 +15,182 @@ func IsConvertibleInto(from, into reflect.Type) bool {
 	panic("not yet implemented")
 }
 
-func AsInt64(you interface{}) (int64, bool) {
-	switch y := you.(type) {
-	case int:
-		return int64(y), true
-	case *int:
-		return int64(*y), true
-	case uint:
-		return int64(y), true
-	case *uint:
-		return int64(*y), true
-	case int8:
-		return int64(y), true
-	case *int8:
-		return int64(*y), true
-	case uint8:
-		return int64(y), true
-	case *uint8:
-		return int64(*y), true
-	case int16:
-		return int64(y), true
-	case *int16:
-		return int64(*y), true
-	case uint16:
-		return int64(y), true
-	case *uint16:
-		return int64(*y), true
-	case int32:
-		return int64(y), true
-	case *int32:
-		return int64(*y), true
-	case uint32:
-		return int64(y), true
-	case *uint32:
-		return int64(*y), true
-	case int64:
-		return int64(y), true
-	case *int64:
-		return int64(*y), true
-	case uint64:
-		return int64(y), true
-	case *uint64:
-		return int64(*y), true
+func AsInt64(w reflect.Value) (int64, bool) {
+	switch w.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		return w.Int(), true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64:
+		return int64(w.Uint()), true
 	default:
 		return 0, false
 	}
 }
 
+func convertSlice(v, w reflect.Value) error {
+	if w.Kind() != reflect.Slice {
+		return fmt.Errorf("Failed to convert slice %v into %v",
+			v.Type(), w.Type())
+	}
+
+	l := w.Len()
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		if !v.CanSet() {
+			return fmt.Errorf("cannot set slice: %v", v)
+		}
+		v.Set(reflect.MakeSlice(v.Elem().Type(), l, l))
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Slice {
+		return fmt.Errorf("not a slice: %v", v)
+	}
+	if v.Cap() < l {
+		if v.CanSet() == false {
+			return fmt.Errorf("slice capacity too short, cannot set : %d", v.Cap())
+		}
+		v.Set(reflect.MakeSlice(v.Type(), l, l))
+	}
+	v.SetLen(l)
+	for i := 0; i < l; i++ {
+		err := convertFrom(v.Index(i), w.Index(i))
+		if err != nil {
+			return fmt.Errorf("cannot convert slice index %d, %w", i, err)
+		}
+	}
+	return nil
+}
+
+func convertMap(v, w reflect.Value) error {
+	if w.Kind() != reflect.Map {
+		return fmt.Errorf("Failed to convert map %v into %v",
+			v.Type(), w.Type())
+	}
+	l := w.Len()
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		if !v.CanSet() {
+			return fmt.Errorf("cannot set slice: %v", v)
+		}
+		v.Set(reflect.MakeMapWithSize(v.Elem().Type(), l))
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Map {
+		return fmt.Errorf("not a map: %v", v)
+	}
+	if v.IsNil() {
+		v.Set(reflect.MakeMapWithSize(v.Type(), l))
+	}
+	for _, k := range w.MapKeys() {
+		key := reflect.New(v.Type().Key())
+		err := convertFrom(key, k)
+		if err != nil {
+			return fmt.Errorf("cannot convert map key %w", err)
+		}
+
+		el := reflect.New(v.Type().Elem())
+		err = convertFrom(key, w.MapIndex(k))
+		if err != nil {
+			return fmt.Errorf("cannot convert map value %w", err)
+		}
+		v.SetMapIndex(key.Elem(), el.Elem())
+	}
+	return nil
+}
+
+func convertStruct(v, w reflect.Value) error {
+	if w.Kind() != reflect.Struct {
+		return fmt.Errorf("Failed to convert struct %v into %v",
+			v.Type(), w.Type())
+	}
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		if !v.CanSet() {
+			return fmt.Errorf("cannot set struct ptr: %v", v)
+		}
+		v.Set(reflect.New(v.Elem().Type()))
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		name := strings.ToLower(v.Type().Field(i).Name)
+		for j := 0; j < w.NumField(); j++ {
+			if name == strings.ToLower(w.Type().Field(j).Name) {
+				err := convertFrom( v.Field(i), w.Field(j))
+				if err != nil {
+					return fmt.Errorf("failed to convert filed %s of %v: %w",
+						name, v.Type(), err)
+				}
+				break
+			}
+		}
+
+	}
+	return nil
+}
+
+func convertFrom(v, w reflect.Value) error {
+
+	errConversion := fmt.Errorf("Failed to convert %v into %v",
+		v.Type(), w.Type())
+
+	if w.Kind() == reflect.Ptr {
+		return convertFrom(v, w.Elem())
+	}
+
+	switch v.Kind() {
+	case reflect.Bool:
+		if w.Kind() == reflect.Bool {
+			v.SetBool(w.Bool())
+			return nil
+		}
+	case reflect.String:
+		if w.Kind() == reflect.String {
+			v.SetString(w.String())
+			return nil
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		if i, ok := AsInt64(w); ok {
+			v.SetInt(i)
+			return nil
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64:
+		if i, ok := AsInt64(w); ok {
+			v.SetUint(uint64(i))
+			return nil
+		}
+	case reflect.Float32, reflect.Float64:
+		if w.Kind() == reflect.Float32 ||
+			w.Kind() == reflect.Float64 {
+			v.SetFloat(w.Float())
+			return nil
+		}
+	case reflect.Ptr:
+		v = v.Elem()
+		switch v.Kind() {
+		case reflect.Slice:
+			return convertSlice(v, w)
+		case reflect.Map:
+			return convertMap(v, w)
+		case reflect.Struct:
+			return convertStruct(v, w)
+		default:
+			return convertFrom(v, w)
+		}
+	case reflect.Slice:
+		return convertSlice(v, w)
+	case reflect.Map:
+		return convertMap(v, w)
+	case reflect.Struct:
+		return convertStruct(v, w)
+	}
+
+	return errConversion
+}
+
 // ConvertFrom applies QiMessaging compatibility rules to convert
 // `you` into `me`. Me can be populated with default values.
 func ConvertFrom(me, you interface{}) error {
-
-	errConversion := fmt.Errorf("Failed to convert %s into %s",
-	reflect.TypeOf(you).Name(), reflect.TypeOf(me).Name())
-
-	switch m := me.(type) {
-	case *string:
-		switch y := you.(type) {
-		case string:
-			*m = y
-			return nil
-		case *string:
-			*m = *y
-			return nil
-		default:
-			return errConversion
-		}
-	case *bool:
-		switch y := you.(type) {
-		case bool:
-			*m = y
-			return nil
-		case *bool:
-			*m = *y
-			return nil
-		default:
-			return errConversion
-		}
-	case *int:
-		if in, ok := AsInt64(you); ok {
-			*m = int(in)
-			return nil
-		}
-	case *uint:
-		if in, ok := AsInt64(you); ok {
-			*m = uint(in)
-			return nil
-		}
-	case *int16:
-		if in, ok := AsInt64(you); ok {
-			*m = int16(in)
-			return nil
-		}
-	case *uint16:
-		if in, ok := AsInt64(you); ok {
-			*m = uint16(in)
-			return nil
-		}
-	case *int32:
-		if in, ok := AsInt64(you); ok {
-			*m = int32(in)
-			return nil
-		}
-	case *uint32:
-		if in, ok := AsInt64(you); ok {
-			*m = uint32(in)
-			return nil
-		}
-	case *int64:
-		if in, ok := AsInt64(you); ok {
-			*m = int64(in)
-			return nil
-		}
-	case *uint64:
-		if in, ok := AsInt64(you); ok {
-			*m = uint64(in)
-			return nil
-		}
-	case *float32:
-		switch y := you.(type) {
-		case float32:
-			*m = float32(y)
-			return nil
-		case *float32:
-			*m = float32(*y)
-			return nil
-		case float64:
-			*m = float32(y)
-			return nil
-		case *float64:
-			*m = float32(*y)
-			return nil
-		default:
-			return errConversion
-		}
-	case *float64:
-		switch y := you.(type) {
-		case float32:
-			*m = float64(y)
-			return nil
-		case *float32:
-			*m = float64(*y)
-			return nil
-		case float64:
-			*m = float64(y)
-			return nil
-		case *float64:
-			*m = float64(*y)
-			return nil
-		default:
-			return errConversion
-		}
-	}
-	return errConversion
+	return convertFrom(reflect.ValueOf(me),reflect.ValueOf(you))
 }
 
 // DecodeFrom decodes x from an encoded value of tye `typ`.
--- type/conversion/conversion_test.go
+++ type/conversion/conversion_test.go
@@ -2,12 +2,16 @@ package conversion_test
 
 import (
 	"testing"
+	"reflect"
 
 	"github.com/lugu/qiloop/type/conversion"
 )
 
 func newBool(b bool) *bool       { return &b }
 func newString(s string) *string { return &s }
+
+func newFloat32(f float32) *float32 { return &f }
+func newFloat64(f float64) *float64 { return &f }
 
 func newUint(n uint) *uint       { return &n }
 func newUint8(n uint8) *uint8 { return &n }
@@ -21,53 +25,230 @@ func newInt16(n int16) *int16 { return &n }
 func newInt32(n int32) *int32 { return &n }
 func newInt64(n int64) *int64 { return &n }
 
-func helpTestConvert2(t *testing.T, a interface{}, b interface{}, v int64) {
+func helpTestConvertInt2(t *testing.T, a interface{}, b interface{}, i int64) {
 	err := conversion.ConvertFrom(a, b)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	i, ok := conversion.AsInt64(a)
+	v := reflect.ValueOf(a)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	in, ok := conversion.AsInt64(v)
 	if !ok {
 		panic("not an integer")
 	}
-	if i != v {
-		t.Errorf("invalid conversion: %d sv %d", i, v)
+	if in != i {
+		t.Errorf("invalid conversion: %d sv %d", in, i)
 	}
 }
 
-func helpTestConvert(t *testing.T, a interface{}) {
+func helpTestConvertInt(t *testing.T, a interface{}) {
 
 	values := []int64 {
 		0, 1, 11, 111, 127,
 	}
 	for _, v := range values {
-		helpTestConvert2(t, a, interface{}(newInt(int(v))), v)
-		helpTestConvert2(t, a, int(v), v)
-		helpTestConvert2(t, a, interface{}(newUint(uint(v))), v)
-		helpTestConvert2(t, a, uint(v), v)
-		helpTestConvert2(t, a, interface{}(newInt8(int8(v))), v)
-		helpTestConvert2(t, a, int8(v), v)
-		helpTestConvert2(t, a, interface{}(newUint8(uint8(v))), v)
-		helpTestConvert2(t, a, uint8(v), v)
-		helpTestConvert2(t, a, interface{}(newInt16(int16(v))), v)
-		helpTestConvert2(t, a, int16(v), v)
-		helpTestConvert2(t, a, interface{}(newUint16(uint16(v))), v)
-		helpTestConvert2(t, a, uint16(v), v)
-		helpTestConvert2(t, a, interface{}(newInt32(int32(v))), v)
-		helpTestConvert2(t, a, int32(v), v)
-		helpTestConvert2(t, a, interface{}(newUint32(uint32(v))), v)
-		helpTestConvert2(t, a, uint32(v), v)
-		helpTestConvert2(t, a, interface{}(newInt64(int64(v))), v)
-		helpTestConvert2(t, a, int64(v), v)
-		helpTestConvert2(t, a, interface{}(newUint64(uint64(v))), v)
-		helpTestConvert2(t, a, uint64(v), v)
+		helpTestConvertInt2(t, a, interface{}(newInt(int(v))), v)
+		helpTestConvertInt2(t, a, int(v), v)
+		helpTestConvertInt2(t, a, interface{}(newUint(uint(v))), v)
+		helpTestConvertInt2(t, a, uint(v), v)
+		helpTestConvertInt2(t, a, interface{}(newInt8(int8(v))), v)
+		helpTestConvertInt2(t, a, int8(v), v)
+		helpTestConvertInt2(t, a, interface{}(newUint8(uint8(v))), v)
+		helpTestConvertInt2(t, a, uint8(v), v)
+		helpTestConvertInt2(t, a, interface{}(newInt16(int16(v))), v)
+		helpTestConvertInt2(t, a, int16(v), v)
+		helpTestConvertInt2(t, a, interface{}(newUint16(uint16(v))), v)
+		helpTestConvertInt2(t, a, uint16(v), v)
+		helpTestConvertInt2(t, a, interface{}(newInt32(int32(v))), v)
+		helpTestConvertInt2(t, a, int32(v), v)
+		helpTestConvertInt2(t, a, interface{}(newUint32(uint32(v))), v)
+		helpTestConvertInt2(t, a, uint32(v), v)
+		helpTestConvertInt2(t, a, interface{}(newInt64(int64(v))), v)
+		helpTestConvertInt2(t, a, int64(v), v)
+		helpTestConvertInt2(t, a, interface{}(newUint64(uint64(v))), v)
+		helpTestConvertInt2(t, a, uint64(v), v)
 	}
 }
 
-func TestBasicConversion(t *testing.T) {
-	helpTestConvert(t, interface{}(newInt(1)))
-	helpTestConvert(t, interface{}(newInt16(1)))
-	helpTestConvert(t, interface{}(newInt32(1)))
-	helpTestConvert(t, interface{}(newInt64(1)))
+func TestInt(t *testing.T) {
+	helpTestConvertInt(t, interface{}(newInt(1)))
+	helpTestConvertInt(t, interface{}(newInt16(1)))
+	helpTestConvertInt(t, interface{}(newInt32(1)))
+	helpTestConvertInt(t, interface{}(newInt64(1)))
+}
+
+func TestString(t *testing.T) {
+	str := newString("")
+	err := conversion.ConvertFrom(str, "boom")
+	if err != nil {
+		t.Error(err)
+	}
+	if *str != "boom" {
+		t.Errorf("failed to convert boom")
+	}
+	err = conversion.ConvertFrom(str, newString("bam"))
+	if err != nil {
+		t.Error(err)
+	}
+	if *str != "bam" {
+		t.Errorf("failed to convert bam")
+	}
+}
+
+func TestBool(t *testing.T) {
+	b := newBool(true)
+	err := conversion.ConvertFrom(b, false)
+	if err != nil {
+		t.Error(err)
+	}
+	if *b != false {
+		t.Errorf("failed to convert false")
+	}
+	*b = true
+	err = conversion.ConvertFrom(b, newBool(false))
+	if err != nil {
+		t.Error(err)
+	}
+	if *b != false {
+		t.Errorf("failed to convert false")
+	}
+}
+
+func TestFloat(t *testing.T) {
+	f := newFloat32(0)
+	err := conversion.ConvertFrom(f, float32(1.2))
+	if err != nil {
+		t.Error(err)
+	}
+	if *f < 1.1 || *f > 1.3 {
+		t.Errorf("failed to convert 1.2")
+	}
+	*f = 0.0
+	err = conversion.ConvertFrom(f, float64(1.2))
+	if err != nil {
+		t.Error(err)
+	}
+	if *f < 1.1 || *f > 1.3 {
+		t.Errorf("failed to convert 1.2")
+	}
+	d := newFloat32(0)
+	err = conversion.ConvertFrom(d, float32(1.2))
+	if err != nil {
+		t.Error(err)
+	}
+	if *d < 1.1 || *d > 1.3 {
+		t.Errorf("failed to convert 1.2")
+	}
+	*d = 0.0
+	err = conversion.ConvertFrom(d, float64(1.2))
+	if err != nil {
+		t.Error(err)
+	}
+	if *d < 1.1 || *d > 1.3 {
+		t.Errorf("failed to convert 1.2")
+	}
+}
+
+func TestSlice(t *testing.T) {
+	from := make([]int32, 5)
+	to := []int{
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	}
+	err := conversion.ConvertFrom(&from, to)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(from) != len(to) {
+		t.Errorf("from size: %d, %d", len(from), len(to))
+	}
+}
+
+func TestMap(t *testing.T) {
+	from := make(map[int8]int16, 5)
+	to := map[int32]int64{
+		1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10,
+	}
+	err := conversion.ConvertFrom(&from, to)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(from) != len(to) {
+		t.Errorf("from size: %d, %d", len(from), len(to))
+	}
+}
+
+type StructFrom struct {
+	A int8
+	B int16
+	C string
+	D float32
+	E bool
+}
+
+type StructTo struct {
+	D float64
+	e bool
+	B int32
+	C string
+	F bool
+}
+
+func TestStruct(t *testing.T) {
+	from := &StructFrom{
+		A: 22,
+	}
+	to := StructTo {
+		D: 1.2,
+		e: true,
+		B: 15,
+		C: "boom",
+		F: true,
+	}
+	err := conversion.ConvertFrom(from, to)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if from.A != 22 {
+		t.Errorf("A should not move")
+	}
+	if int32(from.B) != to.B {
+		t.Errorf("B not the same: %#v", from)
+	}
+	if from.C != to.C {
+		t.Errorf("C not the same: %#v", from)
+	}
+	if float64(from.D) < to.D - 0.1 || float64(from.D) > to.D + 0.1 {
+		t.Errorf("D not in range: %#v", from)
+	}
+	if from.E != to.e {
+		t.Errorf("E not in range: %#v", from)
+	}
+}
+
+func TestStructSlice(t *testing.T) {
+	from := make([]StructFrom, 2)
+	to := []StructTo {
+		StructTo {
+			D: 1.2,
+			e: true,
+			B: 15,
+			C: "boom",
+			F: true,
+		}, StructTo {
+			D: 1.2,
+			e: true,
+			B: 15,
+			C: "boom",
+			F: true,
+		},
+	}
+
+
+	err := conversion.ConvertFrom(&from, to)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
